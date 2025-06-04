6481f86c25e038247b118c00299a915680691b7f
charon616 <milkyway.k16@gmail.com>
2025-06-04 12:52:24 +0100

    add commonjs package

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCommonjs()
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss], //追加
